03b339b3123ce2cedf46077b8651d5d4e425f4a2
sarthak <sarthak@hiddenlayer.com>
2024-02-07 01:54:18 +0530

    :

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify, render_template, json 
+import pickle
+import numpy as np
+from flask_cors import CORS
+import timm
+import base64
+import requests
+
+app = Flask(__name__)
+CORS(app)
+
+model = timm.create_model('mobilenetv3_large_100.ra_in1k', pretrained=True)
+model = model.eval()
+data_config = timm.data.resolve_model_data_config(model)
+transforms = timm.data.create_transform(**data_config, is_training=False)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    img = request.form.get('image')
+    print(img)
+    # print("image = url" + img)
+    # img =  base64.b64decode(request.get_json()['image'])
+    # print("run")
+    return jsonify({'prediction': 1})
+    # print(img)
+    # # exit()
+    # if img is not None:
+    #     print('meow')
+    #     prediction = model(transforms(img).unsqueeze(0))
+    #     return jsonify({'prediction': int(prediction)})
+    # else:
+    #     print('woof')
+    #     return jsonify({'error': 'bruski.'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- popup.js
+++ popup.js
@@ -1,67 +1,73 @@
 document.getElementById('coverImg').addEventListener('click', async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: coverImage,
-    });
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: coverImage,
   });
-
-
+});
 
 async function coverImage() {
-    const sess = new onnx.InferenceSession();
-    const modelPath = "model.onnx";
-    await sess.loadModel(modelPath);  `
+  // const sess = new onnx.InferenceSession();
+  // const modelPath = "model.onnx";
+  // await sess.loadModel(modelPath);  
 
-    let images = document.getElementsByTagName('img');
-    for (let img of images) {
-                let rect = img.getBoundingClientRect();
-                let div = document.createElement('div');
-                div.style.position = 'absolute';
-                div.style.top = rect.top + 'px';
-                div.style.left = rect.left + 'px';
-                div.style.width = rect.width + 'px';
-                div.style.height = rect.height + 'px';
-                div.style.backgroundColor = 'rgba(255, 0, 0, 0.5)';
-                div.style.zIndex = 1000;
+  let images = document.getElementsByTagName('img');
+  for (let img of images) {
+              let rect = img.getBoundingClientRect();
+              let div = document.createElement('div');
+              div.style.position = 'absolute';
+              div.style.top = rect.top + 'px';
+              div.style.left = rect.left + 'px';
+              div.style.width = rect.width + 'px';
+              div.style.height = rect.height + 'px';
+              div.style.backgroundColor = 'rgba(255, 0, 0, 0.5)';
+              div.style.zIndex = 1000;
 
-                document.body.appendChild(div);
-    }
+              document.body.appendChild(div);
+  }
+        
+  for (let img of images) {
+      let rect = img.getBoundingClientRect();
+      let div = document.createElement('div');
+      div.style.position = 'absolute';
+      div.style.top = rect.top + 'px';
+      div.style.left = rect.left + 'px';
+      div.style.width = rect.width + 'px';
+      div.style.height = rect.height + 'px';
+      div.style.backgroundColor = 'rgba(0, 0, 255, 1)';
+      div.style.zIndex = 1000;
+      document.body.appendChild(div);
+      console.log('this is happening')
+      // try {
+      const session = await ort.InferenceSession.create('./model.onnx');
+      console.log('model loaded')
+      const dims = [1, 3, 224, 224];
+      console.log('dims loaded')
+      const size = dims[0] * dims[1] * dims[2] * dims[3];
+      console.log('size loaded')
+      const inputData = Float32Array.from({ length: size }, () => Math.random());
+      console.log('inputData loaded')
+      const feeds = { input1: new ort.Tensor('float32', inputData, dims) };
+      console.log('feeds loaded')
+      // feed inputs and run
+      const results = await session.run(feeds);
+      console.log(results.output1.data);
+      // } catch (e) {
+      //   console.log(e);
+      // }
+      // Load the pre-processor image using HTML5 Canvas
+      // const model_input = new onnx.Tensor(new Float32Array(img.data), "float32");
 
-    // await sess.loadModel("./model.onnx");
-            
-    for (let img of images) {
-        let rect = img.getBoundingClientRect();
-        let div = document.createElement('div');
-        div.style.position = 'absolute';
-        div.style.top = rect.top + 'px';
-        div.style.left = rect.left + 'px';
-        div.style.width = rect.width + 'px';
-        div.style.height = rect.height + 'px';
-        div.style.backgroundColor = 'rgba(0, 0, 255, 1)';
-        div.style.zIndex = 1000;
-        document.body.appendChild(div);
-    
-        // Load the pre-processor image using HTML5 Canvas
-        let imgCanvas = document.createElement('canvas');
-        imgCanvas.width = img.width;
-        imgCanvas.height = img.height;
+      // ONNX model execution
+      // const start = performance.now();
+      // const output =  await session.then(s => { return s.run(model_input,['output_0'])});
+      // const duration = (performance.now() - start).toFixed(1);
 
-        let ctx = imgCanvas.getContext('2d');
-        // ctx.drawImage(img, 0, 0, img.width, img.height);
-
-        // Input data preprocessing, depending on your model's requirement. assuming grayscale and normalize
-        let pixels = ctx.getImageData(0, 0, img.width, img.height);
-        // let inputData = new Float32Array(pixels.data.length / 4);
-        // for (let i = 0; i < pixels.data.length; i += 4) {
-        //     inputData[i / 4] = (pixels.data[i] * 0.299 + pixels.data[i + 1] * 0.587 + pixels.data[i + 2] * 0.114) / 255;
-        // }
-        const input = new onnx.Tensor(new Float32Array(pixels.data), "float32");
-
-        // ONNX model execution
-        const outputMap = sess.run([input]);
-        const outputData = outputMap.values().next().value.data;  // Use your model's output
-        const predictions = outputData.data;
-        const maxPrediction = Math.max(...predictions);
-    }
+  }
 }
+
+
+
+
+
+
