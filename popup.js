b309df78384148288618d4047ede27e75d41c77d
sarthak <sarthak@hiddenlayer.com>
2024-03-04 17:40:10 +0530

    update code to import ort

--- popup.js
+++ popup.js
@@ -1,67 +1,61 @@
-import * as ort from 'onnxruntime-web';
-
 document.getElementById('coverImg').addEventListener('click', async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  chrome.scripting.executeScript({
+  imgs = await chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: coverImage,
+    function: get_imgs,
   });
+  console.log("meow1");
+  await coverImage(imgs);
+  console.log("meow3");
 });
 
-async function coverImage() {
+async function get_imgs() {
   let images = document.getElementsByTagName('img');
   for (let img of images) {
-              let rect = img.getBoundingClientRect();
-              let div = document.createElement('div');
-              div.style.position = 'absolute';
-              div.style.top = rect.top + 'px';
-              div.style.left = rect.left + 'px';
-              div.style.width = rect.width + 'px';
-              div.style.height = rect.height + 'px';
-              div.style.backgroundColor = 'rgba(255, 0, 0, 0.5)';
-              div.style.zIndex = 1000;
+    let rect = img.getBoundingClientRect();
+    let div = document.createElement('div');
+    div.style.position = 'absolute';
+    div.style.top = rect.top + 'px';
+    div.style.left = rect.left + 'px';
+    div.style.width = rect.width + 'px';
+    div.style.height = rect.height + 'px';
+    div.style.backgroundColor = 'rgba(255, 0, 0, 0.5)';
+    div.style.zIndex = 1000;
 
-              document.body.appendChild(div);
+    document.body.appendChild(div);
   }
-        
+
+  return images;
+}
+
+async function coverImage(images) {
   for (let img of images) {
-      let rect = img.getBoundingClientRect();
-      let div = document.createElement('div');
-      div.style.position = 'absolute';
-      div.style.top = rect.top + 'px';
-      div.style.left = rect.left + 'px';
-      div.style.width = rect.width + 'px';
-      div.style.height = rect.height + 'px';
-      div.style.backgroundColor = 'rgba(0, 0, 255, 1)';
-      div.style.zIndex = 1000;
-      document.body.appendChild(div);
-      // getting this log statement in the console
-      console.log('this is happening')
-      // try {
+      // let rect = img.getBoundingClientRect();
+      // let div = document.createElement('div');
+      // div.style.position = 'absolute';
+      // div.style.top = rect.top + 'px';
+      // div.style.left = rect.left + 'px';
+      // div.style.width = rect.width + 'px';
+      // div.style.height = rect.height + 'px';
+      // div.style.backgroundColor = 'rgba(0, 0, 255, 1)';
+      // div.style.zIndex = 1000;
+      // document.body.appendChild(div);
+      console.log('this is happening');
       const options = {
         executionProviders: ['wasm'], 
         graphOptimizationLevel: 'all'
       };
-      // this is where we try to load the model
-      const session = ort.InferenceSession.create('./mobilenetv2-10.onnx', options);
-      // not getting this log statement in the console, after this we try to do processing etc but after the above line nothing just works, no statement etc, nothing at all
-      console.log('model loaded')
-      const dims = [1, 3, 224, 224];
-      console.log('dims loaded')
-      const size = dims[0] * dims[1] * dims[2] * dims[3];
-      console.log('size loaded')
-      const inputData = Float32Array.from({ length: size }, () => Math.random());
-      console.log('inputData loaded')
-      const feeds = { input1: new ort.Tensor('float32', inputData, dims) };
-      console.log('feeds loaded')
-      // feed inputs and run
-      const results = await session.run(feeds);
-      console.log(results.output1.data);
+      const session = await ort.InferenceSession.create('model.onnx', options);
+      console.log('model loaded');
+      // const dims = [1, 3, 224, 224];
+      // console.log('dims loaded');
+      // const size = dims[0] * dims[1] * dims[2] * dims[3];
+      // console.log('size loaded');
+      // const inputData = Float32Array.from({ length: size }, () => Math.random());
+      // console.log('inputData loaded');
+      // const feeds = { input1: new ort.Tensor('float32', inputData, dims) };
+      // console.log('feeds loaded');
+      // const results = await session.run(feeds);
+      // console.log(results.output1.data);
   }
 }
-
-
-
-
-
-
